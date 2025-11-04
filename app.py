380c9fc480d87cbd1e1ad24d82be3f682a18dcb9
RenanTrevelim <111608172+RenanTrevelim@users.noreply.github.com>
2025-11-04 23:11:09 +0000

    commit

--- app.py
+++ app.py
@@ -1,1 +1,75 @@
 import streamlit as st
+import gdown
+import tensorflow as tf
+import io
+from PIL import Image
+import numpy as np
+import plotly.express as px
+import pandas as pd
+
+@st.cache_resource
+
+def carrega_modelo():
+    url = 'https://drive.google.com/uc?id=1YV-XVuZM6lszstIRDs7gC2oUqQJdS-qX'
+
+    gdown.download(url, 'modelo_quantizado16bits.tflite')
+    interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
+    interpreter.allocate_tensors()
+
+    return interpreter
+
+def carrega_imagem():
+    uploads_file = st.file_uploader('Arraste e solte uma imagem ou clique para selecionar uma', type=['png', 'jpg', 'jpeg'])
+
+    if uploads_file is not None:
+        image_data = uploads_file.read()
+        image = Image.open(io.BytesIO(image_data))
+
+        st.image(image)
+        st.success('Imagem carregada com sucesso')
+
+        image = np.array(image, dtype=np.float32)
+        image = image / 255.0
+        image = np.expand_dims(image, axis=0)
+
+        return image
+    
+def previsao(interpreter, image):
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], image)
+
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    classes = ['BlackMeasles', 'BlackRot', 'HealthyGrapes', 'LeafBlight']
+
+    df = pd.DataFrame()
+    df['classes'] = classes
+    df['probabilidades (%)'] = 100*output_data[0]
+    
+    fig = px.bar(df,y='classes',x='probabilidades (%)',  orientation='h', text='probabilidades (%)', title='Probabilidade de Classes de Doenças em Uvas')
+    st.plotly_chart(fig)
+
+def main():
+    st.set_page_config(
+        page_title='Classifica folhas de videira'
+    )
+    st.write('# Classifica Folhas de Videira!')
+    # carregar o modelo
+
+    interpreter = carrega_modelo()
+
+    # carrega imagem
+
+    image = carrega_imagem()
+
+    # classifica
+
+    if image is not None:
+        previsao(interpreter, image)
+
+if __name__ == "__main__":
+    main()
